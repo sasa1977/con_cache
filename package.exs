028181168c2efb9df30275302c53f17e9807d12a
sasa1977 <sasa.juric@gmail.com>
2013-04-17 10:11:23 +0200

    expm

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(
+  name: "con_cache", description: "ets based key/value cache with row level isolated writes and ttl support",
+  version: "0.1", keywords: ["elixir", "ets", "cache"], 
+  maintainers: [[name: "Sasa Juric", email: "sasa.juric@gmail.com"]],
+  repositories: [[github: "sasa1977/con_cache"]]
+)
