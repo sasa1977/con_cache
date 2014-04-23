0e9aae58b0d7ffd7716e331bce819955f12551eb
Sasa Juric <sasa.juric@gmail.com>
2014-04-23 14:14:49 +0200

    hex manifest

--- mix.exs
+++ mix.exs
@@ -2,10 +2,21 @@ defmodule Lock.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :con_cache,
+    [
+      app: :con_cache,
       version: "0.0.1",
       elixir: ">= 0.13.0",
-      deps: deps ]
+      deps: deps,
+      package: [
+        contributors: ["Saša Jurić"],
+        licenses: ["MIT"],
+        links: [{"Github", "https://github.com/sasa1977/con_cache"}]
+      ],
+      description: """
+        ETS based key-value storage with support for row-level isolated writes, TTL auto-purge,
+        and modification callbacks.
+      """
+    ]
   end
 
   def application do
