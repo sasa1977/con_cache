5e4dcaa641a67b8a978ec0f2df9ba23bc35d0ffc
Sasa Juric <sasa.juric@gmail.com>
2014-09-02 22:29:51 +0200

    Elixir v1.0.0-rc1

--- mix.exs
+++ mix.exs
@@ -3,18 +3,18 @@ Code.ensure_loaded?(Hex) and Hex.start
 defmodule ConCache.Mixfile do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.5.0"
 
   def project do
     [
       app: :con_cache,
       version: @version,
-      elixir: "~> 0.15.0",
+      elixir: "~> 1.0.0-rc1",
       deps: deps,
       package: [
         contributors: ["Saša Jurić"],
         licenses: ["MIT"],
-        links: [{"Github", "https://github.com/sasa1977/con_cache"}]
+        links: %{"Github": "https://github.com/sasa1977/con_cache"}
       ],
       description: "ETS based key-value storage with support for row-level isolated writes, TTL auto-purge, and modification callbacks.",
       docs: [
@@ -27,12 +27,12 @@ defmodule ConCache.Mixfile do
   end
 
   def application do
-    [applications: [:exactor], mod: {ConCache.Application, []}]
+    [applications: [:logger, :exactor], mod: {ConCache.Application, []}]
   end
 
   defp deps do
     [
-      {:exactor, "~> 0.6.0"},
+      {:exactor, "~> 0.7.0"},
       {:ex_doc, github: "elixir-lang/ex_doc", only: :docs}
     ]
   end
