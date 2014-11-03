492aa5025e6705798828062e6faaf530f71f28ba
Low Kian Seong <kianseong@gmail.com>
2014-11-03 10:06:30 +0800

    Update mix.exs
    links in `mix.exs` is now a map

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule ConCache.Mixfile do
       package: [
         contributors: ["Saša Jurić"],
         licenses: ["MIT"],
-        links: %{"Github": "https://github.com/sasa1977/con_cache"}
+        links: %{"Github" => "https://github.com/sasa1977/con_cache"}
       ],
       description: "ETS based key-value storage with support for row-level isolated writes, TTL auto-purge, and modification callbacks.",
       docs: [
