2a55ad53c8ba38a7169cac9b1d8834cda1439401
Sasa Juric <sasa.juric@gmail.com>
2018-04-02 11:26:35 +0200

    require Elixir 1.5+

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule ConCache.Mixfile do
     [
       app: :con_cache,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
