5d729efe1a367eb66474ee39404b0f345512e381
Sasa Juric <sasa.juric@gmail.com>
2018-04-29 12:40:28 +0200

    add dialyzer

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule ConCache.Mixfile do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.14.0", only: :dev}
+      {:ex_doc, "~> 0.14.0", only: :dev},
+      {:dialyxir, "~> 0.5.0", only: :dev}
     ]
   end
 end
