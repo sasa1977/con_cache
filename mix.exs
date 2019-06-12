158919c7151035e728457b04b62322afd4f00aec
Saša Jurić <sasa.juric@gmail.com>
2019-06-12 17:52:51 +0200

    increase minimum Elixir version

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule ConCache.Mixfile do
     [
       app: :con_cache,
       version: @version,
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
